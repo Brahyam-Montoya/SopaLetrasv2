273109ea9a861524dd5bad18130152c1b361ab7b
Brahyam-Montoya <bmontoyagaviriamail.com>
2022-05-30 20:41:02 -0500

    first commit

---
+++ codigo.js
@@ -0,0 +1,116 @@
+$(document).ready(function(){
+    $(".desarrollo").hover(function(){
+        $(".desarrollo").addClass('resaltar');
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".etiqueta").hover(function(){
+        $(".etiqueta").addClass('resaltarVerde');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".servicio").hover(function(){
+        $(".servicio").addClass('resaltarVerde');
+    });
+});
+
+$(document).ready(function(){
+    $(".logica").hover(function(){
+        $(".logica").addClass('resaltarVerde');
+    });
+});
+
+$(document).ready(function(){
+    $(".servidor").hover(function(){
+        $(".servidor").addClass('resaltar');
+    });
+});
+
+$(document).ready(function(){
+    $(".servidores").hover(function(){
+        $(".servidores").addClass('resaltarNaranja');
+    });
+});
+$(document).ready(function(){
+    $(".dom").hover(function(){
+        $(".dom").addClass('resaltar');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".figma").hover(function(){
+        $(".figma").addClass('resaltar');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".editor").hover(function(){
+        $(".editor").addClass('resaltarVerde');
+    });
+});
+
+$(document).ready(function(){
+    $(".datos").hover(function(){
+        $(".datos").addClass('resaltarVerde');
+    });
+});
+
+$(document).ready(function(){
+    $(".html").hover(function(){
+        $(".html").addClass('resaltar');
+    });
+});
+
+$(document).ready(function(){
+    $(".estructura").hover(function(){
+        $(".estructura").addClass('resaltarNaranja');
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".url").hover(function(){
+        $(".url").addClass('resaltar');
+    });
+});
+
+$(document).ready(function(){
+    $(".clientes").hover(function(){
+        $(".clientes").addClass('resaltarVerde');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".red").hover(function(){
+        $(".red").addClass('resaltarVerde');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".codigor").hover(function(){
+        $(".codigor").addClass('resaltarVerde');
+    });
+});
+
+$(document).ready(function(){
+    $(".tablass").hover(function(){
+        $(".tablass").addClass('resaltarNaranja');
+    });
+});
+
+
+$(document).ready(function(){
+    $(".tablass").hover(function(){
+        $(".bloque").addClass('resaltar');
+    });
+});
